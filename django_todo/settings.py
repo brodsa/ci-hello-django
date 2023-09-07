4ace15bbc6f58153bd793fc993d52caef5679c03
brodsa <brodinova@gmail.com>
2023-09-07 09:35:24 +0000

    feat: change the title of the homepage

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,16 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_0#!t&rro&$o@c$_0pk86%^c=s2h$zlz$6c*2_3!#76f)+1g&z' 
-# SECRET_KEY =  os.environ['SECRET_KEY'] 
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-_0#!t&rro&$o@c$_0pk86%^c=s2h$zlz$6c*2_3!#76f)+1g&z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ci-django-todo-app-sg-9a3b0ebffe20.herokuapp.com',
-    '8000-brodsa-cihellodjango-3udabb6zj7d.ws-eu104.gitpod.io'
+    os.environ.get("HEROKU_HOST"),
+    os.environ.get("GITPOD_HOST")
     ]
 
 
